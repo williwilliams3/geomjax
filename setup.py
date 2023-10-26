8e915e56106fb21acd533c4b280fa13fe5cd49d2
williwilliams3 <williwilliams3@hotmail.com>
2023-10-26 16:37:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache License",
         "Operating System :: OS Independent",
     ],
 )
