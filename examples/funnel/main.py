24e33c75a663533e92738af9bdf808bfa44fd770
williwilliams3 <williwilliams3@hotmail.com>
2023-10-26 16:19:09 +0300

    Add example

---
+++ examples/funnel/main.py
@@ -0,0 +1,122 @@
+import jax
+import jax.numpy as jnp
+import jax.scipy.stats as jss
+import geomjax
+
+# import matplotlib.pyplot as plt
+
+
+def inference_loop(rng_key, sampler, initial_position, num_samples):
+    initial_state = sampler.init(initial_position)
+
+    kernel = sampler.step
+
+    @jax.jit
+    def one_step(state, rng_key):
+        state, _ = kernel(rng_key, state)
+        return state, state
+
+    keys = jax.random.split(rng_key, num_samples)
+    _, states = jax.lax.scan(one_step, initial_state, keys)
+
+    return states
+
+
+class neal_funnel:
+    def __init__(self, D=2, mean=0.0, sigma=3.0):
+        self.sigma = sigma
+        self.mean = mean
+        self.D = D
+        self.name = "NealFunnel"
+        self.xlim = [-10.0, 10.0]
+        self.ylim = [-10.0, 10.0]
+
+    def logp(self, theta):
+        return jss.norm.logpdf(theta[self.D - 1], loc=0.0, scale=self.sigma) + jnp.sum(
+            jss.norm.logpdf(
+                theta[: self.D - 1], loc=0.0, scale=jnp.exp(0.5 * theta[self.D - 1])
+            )
+        )
+
+    def inverse_jacobian(self, theta):
+        D = self.D
+        upper_rows = jnp.c_[
+            jnp.exp(-0.5 * theta[-1]) * jnp.eye(D - 1),
+            -0.5 * jnp.exp(-0.5 * theta[-1]) * theta[0 : (D - 1)],
+        ]
+        lowest_row = jnp.append(jnp.zeros(D - 1), 1.0 / self.sigma)
+        inverse_jacobian = jnp.r_[upper_rows, [lowest_row]]
+        return inverse_jacobian
+
+    def fisher_metric_fn(self, theta):
+        inverse_jacobian = self.inverse_jacobian(theta)
+        metric = inverse_jacobian.T @ inverse_jacobian
+        return 0.5 * (metric + metric.T)
+
+
+if __name__ == "__main__":
+    # Build the kernel
+    # LMC Monge
+    M = neal_funnel()
+    logdensity_fn = lambda x: M.logp(x)
+    metric_fn = lambda x: M.fisher_metric_fn(x)
+    step_size = 1e-1
+    inverse_mass_matrix = jnp.ones(M.D)
+    num_integration_steps = 8
+    alpha2 = 1.0
+    sampler = geomjax.lmcmonge(
+        logdensity_fn,
+        step_size,
+        inverse_mass_matrix,
+        num_integration_steps,
+        alpha2=alpha2,
+    )
+
+    # Initialize the state
+    initial_position = jnp.ones(M.D)
+    state = sampler.init(initial_position)
+
+    # Iterate
+    rng_key = jax.random.PRNGKey(0)
+    step = jax.jit(sampler.step)
+    # 1 step
+    state, info = step(rng_key, state)
+    print("State", state)
+    print("Info", info)
+    print("------------------------------\n")
+    # Sample 1 chain
+    states = inference_loop(
+        rng_key,
+        sampler,
+        initial_position,
+        num_samples=1000,
+    )
+    # plt.scatter(states.position[:, 0], states.position[:, 1])
+    # plt.grid()
+    # plt.show()
+
+    # LMC Fisher
+    sampler = geomjax.nutslmc(logdensity_fn, step_size, metric_fn)
+
+    # Initialize the state
+    initial_position = jnp.ones(M.D)
+    state = sampler.init(initial_position)
+    print("Ini state", state)
+
+    # Iterate
+    rng_key = jax.random.PRNGKey(42)
+    step = jax.jit(sampler.step)
+    # 1 step
+    state, info = step(rng_key, state)
+    print("State", state)
+    print("Info", info)
+    # Sample 1 chain
+    states = inference_loop(
+        rng_key,
+        sampler,
+        initial_position,
+        num_samples=1000,
+    )
+    # plt.scatter(states.position[:, 0], states.position[:, 1])
+    # plt.grid()
+    # plt.show()
